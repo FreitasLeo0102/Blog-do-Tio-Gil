d9ed94012609e00990af01d5ceff105313b59fa5
FreitasLeo0102 <freitasleo535@gmail.com>
2025-12-04 12:18:54 -0300

    Refactor user profile functions and data structure

--- JavaScript/perfil.js
+++ JavaScript/perfil.js
@@ -28,9 +28,7 @@ function getUserData() {
             seguidores: "500M",
             banner: "../IMG/Post-Perfil/Perfil/tali/banner-tali.png",
             pfp: "../IMG/Post-Perfil/Perfil/tali/fotoperfil-tali.png",
-            post: "Saindo um pouco do foco de Lumity, esse último episódio de The Owl House dói até a alma. Foi nele que percebi, de verdade, que a série havia terminado e que não teríamos mais uma nova temporada. Nunca mais veremos todos eles juntos novamente 😭💔",
-            img: "../IMG/Post-Perfil/WhatsApp Image 2025-12-04 at 11.57.11.jpeg"
-
+            post: "Saindo um pouco do foco de Lumity, esse último episódio de The Owl House dói até a alma. Foi nele que percebi, de verdade, que a série havia terminado e que não teríamos mais uma nova temporada. Nunca mais veremos todos eles juntos novamente 😭💔"
         },
 
         mary: {
@@ -117,6 +115,7 @@ function main() {
 }
 
 main();
+
 
 
 
